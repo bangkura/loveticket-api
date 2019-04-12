03d6c6f9ca72bc6422f1de44823c9bb458f75569
jiatengyang <tengyangjia@gmail.com>
2019-04-12 14:20:25 +0800

    create the project

---
+++ src/main/java/com/bangkura/loveticket/LoveticketApplication.java
@@ -0,0 +1,13 @@
+package com.bangkura.loveticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveticketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveticketApplication.class, args);
+    }
+
+}

31776fe675cfff908a7b3525fc3c830851ec15c4
bangkura <tengyangjia@gmail.com>
2019-04-13 11:50:59 +0800

    change the controller

--- src/main/java/com/bangkura/loveticket/Controllers/TestController.java
+++ src/main/java/com/bangkura/loveticket/Controllers/TestController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/sayhello")
     public String sayHello() {
         return "hello";
     }

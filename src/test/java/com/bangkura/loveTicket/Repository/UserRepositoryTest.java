cfdd732e6667ba9101ba18546a031b3580cef1a3
bangkura <tengyangjia@gmail.com>
2019-04-15 18:18:09 +0800

    remove the sonar step of the jenkins file temporarily for performance

--- src/test/java/com/bangkura/loveTicket/Repository/UserRepositoryTest.java
+++ src/test/java/com/bangkura/loveTicket/Repository/UserRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.bangkura.loveTicket.Repository;
 
 import com.bangkura.loveTicket.Model.User;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +25,23 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
+    @Before
+    public void setUp() {
+        User user = new User();
+        user.setAge(12);
+        user.setName("name");
+        userRepository.save(user);
+    }
+
     @Test
     public void testAdd() {
+        User user = userRepository.findByAge(12);
+        Assert.assertTrue(user.getAge() == 12);
+    }
+
+    @After
+    public void cleanUp() {
+        User user = userRepository.findByAge(12);
+        userRepository.delete(user);
     }
 }
